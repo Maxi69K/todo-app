15f2bec7acb13a078f5156bf01e7a626377bca19
Goran Kekez <gorankekez@gmail.com>
2024-03-08 14:01:54 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
       <footer className="my-footer">
         <small>
-          &copy; 2022 Todo App by <a href="https://webdizajnmaxi.eu.org" target="_blank" rel="noreferrer">Maxi</a>. All Rights Reserved.</small>
+          &copy; 2022 Todo App by <a href="https://webdevelopermaxi.eu.org" target="_blank" rel="noreferrer">Maxi</a>. All Rights Reserved.</small>
       </footer>
     )
 }
